ab8bbffdec594bae8b046e5e07a128cc6661487b
zwwkk <137834838+zwwkk@users.noreply.github.com>
2024-07-01 01:04:26 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+alert("Bénvenu sure soundcloud panele admine");
+
+let letexte = "Pourquoi tu regarde ta console mon pote ?";
+
+console.log(letexte);
+
+let unchiffre = 25;
+unchiffre = 20;
+
+let chaine = "Je suis l'une des chaînes de caractères";
+
+let lautre = "Chaines précédencente: " + chaine + ". Voici le contenu baby";
